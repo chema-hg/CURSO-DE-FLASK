45a933ebdc18711a0cf5f387468840685df69e7f
chema_hg <64953146+chema-hg@users.noreply.github.com>
2021-06-17 23:40:53 +0200

    Update boot.sh

--- POST 22/boot.sh
+++ POST 22/boot.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 # this script is used to boot a Docker container
-# while true; do
-#     flask db upgrade
-#     if [[ "$?" == "0" ]]; then
-#         break
-#     fi
-#     echo Deploy command failed, retrying in 5 secs...
-#     sleep 5
-# done
-flask db upgrade
+
 exec gunicorn -b :5000 inicio:app
